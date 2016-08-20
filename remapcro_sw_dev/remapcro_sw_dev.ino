2b2cb81c59ff9a30c00c9a3eeb962ada3ea7f5b3
Anthony Lieuallen <arantius@gmail.com>
2016-08-20 18:57:03 -0400

    two-part basic data shunt

---
+++ remapcro_sw_dev/remapcro_sw_dev.ino
@@ -0,0 +1,59 @@
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\
+
+// https://forum.arduino.cc/index.php?topic=38107.msg282342#msg282342
+void PrintHex8(uint8_t *data, uint8_t length) {
+   char tmp[length*3+1];
+   memset(tmp, 0x20, length * 3);
+   byte first;
+   byte second;
+   for (int i=0; i<length; i++) {
+     first = (data[i] >> 4) & 0x0f;
+     second = data[i] & 0x0f;
+     // base for converting single digit numbers to ASCII is 48
+     // base for 10-16 to become lower-case characters a-f is 87
+     // note: difference is 39
+     tmp[i*3] = first+48;
+     tmp[i*3+1] = second+48;
+     if (first > 9) tmp[i*3] += 39;
+     if (second > 9) tmp[i*3+1] += 39;
+   }
+   tmp[length*3] = 0;
+   Serial.println(tmp);
+}
+
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\
+
+void setup() {
+  // Debugging out.
+  Serial.begin(74880);
+  // In from host processor.
+  Serial1.begin(74880);  
+
+  while (!Serial) { }
+
+  Serial.println("Device started!");
+}
+
+
+byte buf[16];
+
+void loop() {
+  if (Serial1.available()) {
+    /*
+    Serial1.readBytes(buf, 2);
+    if (buf[0] != 0xD0 || buf[1] != 0x0D) {
+      while (Serial1.available()) Serial1.read();
+    }
+    */
+
+    byte len = Serial1.read();
+    Serial.print("Reading bytes:");
+    Serial.println(len, DEC);
+
+    if (len) {
+      Serial1.readBytes(buf, len);
+      PrintHex8(buf, len);
+    }
+  }
+}
+
---
+++ remapcro_sw_host/remapcro_sw_host.ino
@@ -0,0 +1,89 @@
+#include <hidboot.h>
+#include <hidcomposite.h>
+
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\
+
+// https://forum.arduino.cc/index.php?topic=38107.msg282342#msg282342
+void PrintHex8(uint8_t *data, uint8_t length) {
+   char tmp[length*3+1];
+   memset(tmp, 0x20, length * 3);
+   byte first;
+   byte second;
+   for (int i=0; i<length; i++) {
+     first = (data[i] >> 4) & 0x0f;
+     second = data[i] & 0x0f;
+     // base for converting single digit numbers to ASCII is 48
+     // base for 10-16 to become lower-case characters a-f is 87
+     // note: difference is 39
+     tmp[i*3] = first+48;
+     tmp[i*3+1] = second+48;
+     if (first > 9) tmp[i*3] += 39;
+     if (second > 9) tmp[i*3+1] += 39;
+   }
+   tmp[length*3] = 0;
+   Serial.println(tmp);
+}
+
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\
+
+KeyboardReportParser keyboardReportParser;
+USB Usb;
+
+
+class HIDSelector : public HIDComposite {
+  public:
+    HIDSelector(USB *p) : HIDComposite(p) {};
+  protected:
+    void ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id, uint8_t len, uint8_t *buf);
+    bool SelectInterface(uint8_t iface, uint8_t proto);
+};
+
+
+bool HIDSelector::SelectInterface(uint8_t iface, uint8_t proto) {
+  if (proto != 0) {
+    return true;
+  }
+  return false;
+}
+
+
+void HIDSelector::ParseHIDData(
+    USBHID *hid, uint8_t ep, bool is_rpt_id, uint8_t len, uint8_t *buf) {
+  digitalWrite(13, !digitalRead(13));
+
+  // Default report parser handles LED twiddling.
+  keyboardReportParser.Parse(hid, is_rpt_id, len, buf);
+
+  // Shunt data to co-processor.
+  //Serial.write(0xD0);
+  //Serial.write(0x0D);
+  Serial.write((byte)len);
+  Serial.write(buf, len);
+}
+
+HIDSelector hidSelector(&Usb);
+
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\
+
+void setup() {
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  
+  Serial.begin(74880);
+
+  if (Usb.Init() == -1) {
+    digitalWrite(13, LOW);
+  }
+  
+  // Set this to higher values to enable more debug information
+  // minimum 0x00, maximum 0xff, default 0x80
+  UsbDEBUGlvl = 0xff;
+
+  delay( 200 );
+}
+
+
+void loop() {
+  Usb.Task();
+}
+
